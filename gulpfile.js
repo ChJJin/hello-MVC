76e90e2227f5a86b8c4a15079ec0cef6c2955477
ChJJin <308366504@qq.com>
2014-06-25 14:19:12 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat'),
+    header = require('gulp-header'),
+    footer = require('gulp-footer');
+
+var config = {
+  src: ['./src/!(index.coffee)', './src/index.coffee'],
+  dest: './lib/',
+  destFile: 'MVC.js',
+  header: ['(function(exports){', '\r\n'],
+  footer: ['', '})(exports);']
+};
+
+gulp.task('script', function(){
+  gulp.src(config.src)
+    .pipe(concat(config.destFile, {newLine: "\r\n"}))
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(header(config.header.join('\r\n')))
+    .pipe(footer(config.footer.join('\r\n')))
+    .pipe(gulp.dest(config.dest));
+});
+
+gulp.task('default', ['script']);
