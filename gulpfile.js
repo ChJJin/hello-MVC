12b891ba0fd2ca5ed4a14641ba16b76cd78532c2
ChJJin <308366504@qq.com>
2014-07-22 15:59:04 +0800

    modify scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,19 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    coffee = require('gulp-coffee'),
-    concat = require('gulp-concat'),
-    header = require('gulp-header'),
-    footer = require('gulp-footer');
+    g    = require('gulp-load-plugins')();
 
 var config = {
   src: ['./src/event.coffee', './src/!(event.coffee|index.coffee)', './src/index.coffee'],
   dest: './lib/',
-  destFile: 'MVC.js',
-  header: ['(function(exports){', '\r\n'],
-  footer: ['', '})(exports);']
+  destFile: 'MVC.js'
 };
 
 gulp.task('script', function(){
   gulp.src(config.src)
-    .pipe(concat(config.destFile, {newLine: "\r\n"}))
-    .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(header(config.header.join('\r\n')))
-    .pipe(footer(config.footer.join('\r\n')))
+    .pipe(g.concat(config.destFile, {newLine: "\r\n"}))
+    .pipe(g.coffee({bare: true}).on('error', g.util.log))
+    .pipe(g.wrap([';(function(exports){',
+      '<%= contents %>',
+      '})(exports);'].join('\r\n')))
     .pipe(gulp.dest(config.dest));
 });
 
